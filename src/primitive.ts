69032e28042bfa4dfec00b997ba9d656faea9b48
bayjidmia <mdbayjidmia982@gmail.com>
2026-04-30 22:17:21 +0600

    observing any type behaviour

---
+++ src/primitive.ts
@@ -0,0 +1,16 @@
+//Js-Ts
+// Js -string ,boolean ,number ,null ,undefined
+// Ts -never,unknown,void
+let a: string = "Hello World";
+a = "Hello TypeScript";
+
+let isadmin: boolean = false;
+isadmin = true;
+
+// when we we declare any
+
+let x: any = 10;
+x = "Hello";
+x = true;
+x = null;
+x = undefined;
