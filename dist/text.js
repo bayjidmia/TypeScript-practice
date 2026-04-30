e04a0e973a9e4cb4a398e40c22a360cfafaa503d
bayjidmia <mdbayjidmia982@gmail.com>
2026-04-30 21:56:33 +0600

    added typscript install and setup stage

---
+++ dist/text.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "TypeScript is a powerful programming language that builds on JavaScript by adding static types. It allows developers to catch errors early and improve code quality. With TypeScript, you can write code that is more maintainable and scalable, making it easier to manage large projects. Whether you're a beginner or an experienced developer, learning TypeScript can enhance your coding skills and open up new opportunities in the world of web development.";
+console.log(course);
+//# sourceMappingURL=text.js.map
---
+++ src/text.ts
@@ -0,0 +1,3 @@
+const course: string =
+  "TypeScript is a powerful programming language that builds on JavaScript by adding static types. It allows developers to catch errors early and improve code quality. With TypeScript, you can write code that is more maintainable and scalable, making it easier to manage large projects. Whether you're a beginner or an experienced developer, learning TypeScript can enhance your coding skills and open up new opportunities in the world of web development.";
+console.log(course);
