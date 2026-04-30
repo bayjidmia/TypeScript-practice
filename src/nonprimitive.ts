7bcd50e339accf0710b5666448e92c199f7ef8f3
bayjidmia <mdbayjidmia982@gmail.com>
2026-05-01 00:22:26 +0600

    added nonprimitive type test

---
+++ src/nonprimitive.ts
@@ -0,0 +1,6 @@
+// array ,object
+let bazarlist: string[] = ["apple", "banana", "orange"];
+
+let mixedarr: (string | number)[] = ["apple", 10, "tomato", 2, "orange", 3];
+
+const coordintes: [number, number] = [1, 3];
