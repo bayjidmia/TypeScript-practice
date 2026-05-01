e13358fb03277e9d2051bf617ee67cd59eced7fd
bayjidmia <mdbayjidmia982@gmail.com>
2026-05-01 09:05:17 +0600

    learning object and optional type

--- src/nonprimitive.ts
+++ src/nonprimitive.ts
@@ -4,3 +4,24 @@ let bazarlist: string[] = ["apple", "banana", "orange"];
 let mixedarr: (string | number)[] = ["apple", 10, "tomato", 2, "orange", 3];
 
 const coordintes: [number, number] = [1, 3];
+
+let ourTuple: [number, boolean, string];
+// initialize correctly
+ourTuple = [5, false, "Coding God was here"];
+
+ourTuple.push("Hello World");
+ourTuple.push(10);
+ourTuple.push(true);
+console.log(ourTuple);
+
+// reference type : object
+
+let user: {
+  firstname: string;
+  middlename?: string; // optional type
+  lastname: string;
+} = {
+  firstname: "jhon",
+  //   middlename: "doe",
+  lastname: "smith",
+};
