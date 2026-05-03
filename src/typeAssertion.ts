d6112e6fc0da5d0a29893b67efd1fdb7b025a612
bayjidmia <mdbayjidmia982@gmail.com>
2026-05-03 09:52:18 +0600

    use typeAssertion

--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -2,7 +2,7 @@ let anything: any;
 anything = "mezba";
 anything as string;
 
-const Kgtogram = (input: string | number) => {
+const Kgtogram = (input: string | number): number | string | undefined => {
   if (typeof input === "number") {
     return input * 1000;
   } else if (typeof input === "string") {
@@ -12,7 +12,7 @@ const Kgtogram = (input: string | number) => {
   }
 };
 
-const result1 = Kgtogram(100);
+const result1 = Kgtogram(100) as number;
 console.log(result1);
-const result2 = Kgtogram("100 kg");
+const result2 = Kgtogram("100 kg") as string;
 console.log(result2);
