104a65117e887c26302c460f20671dcaef57c5f5
bayjidmia <mdbayjidmia982@gmail.com>
2026-05-04 08:42:54 +0600

    learn contraint

---
+++ src/contraint.ts
@@ -0,0 +1,19 @@
+interface Student {
+  name: string;
+  age: number;
+}
+
+const leve2student = <T extends Student>(student: T) => {
+  return { enroll: "next level", ...student };
+};
+
+const student1 = { name: "John", age: 20, hascar: true };
+
+const student2 = { name: "Jane", age: 22, hascar: false };
+const student3 = { name: "Doe", age: 25, hascar: true };
+const level2student1 = leve2student(student1);
+console.log(level2student1);
+const level2student2 = leve2student(student2);
+console.log(level2student2);
+const level2student3 = leve2student(student3);
+console.log(level2student3);
