5c5fb597beacda93e7894ef3469e04c750bfab1d
bayjidmia <mdbayjidmia982@gmail.com>
2026-05-04 08:03:49 +0600

    generictype method in function

--- src/genericFunction.ts
+++ src/genericFunction.ts
@@ -14,9 +14,23 @@ const arrString = createArraywithString("hello");
 const arrNumber = createArraywithNumber(42);
 const objWithArr = createobjectwitharray({ name: "bayjid", id: 1 });
 
-console.log(arrString);
-// Output: ["hello"]
-console.log(arrNumber);
-// Output: [42]
-console.log(objWithArr);
-// Output: [{ name: "bayjid", id: 1 }]
+// console.log(arrString);
+// // Output: ["hello"]
+// console.log(arrNumber);
+// // Output: [42]
+// console.log(objWithArr);
+// // Output: [{ name: "bayjid", id: 1 }]
+
+const convator = <T, X>(value: T, value2: X) => {
+  return [value, value2];
+};
+
+const arrStringGeneric = convator("hello", "world");
+console.log(arrStringGeneric);
+const arrNumberGeneric = convator(42, 43);
+console.log(arrNumberGeneric);
+const objWithArrGeneric = convator(
+  { name: "bayjid", id: 1 },
+  { name: "alice", id: 2 },
+);
+console.log(objWithArrGeneric);
